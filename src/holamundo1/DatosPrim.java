009e514da254e4a1af136120c497918fdaa2d759
Fernando Paredes Rios <fprios112@gmail.com>
2019-10-17 17:35:03 -0500

    primeros proyectos previo examen

---
+++ src/holamundo1/DatosPrim.java
@@ -0,0 +1,13 @@
+package holamundo1;
+
+public class DatosPrim {
+    public static void main(String[] args) {
+        //datos no primitivos
+        Integer numero = null;
+
+        String palabra = "Hola a todos"; //siempre en comillas dobles, las simples se usan en caracteres
+
+        System.out.println("El numero es " + numero);
+        System.out.println("La palabra es " + palabra);
+    }
+}
---
+++ src/holamundo1/HolaMundo.java
@@ -0,0 +1,26 @@
+package holamundo1;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Siempre me olvido de Java :v");
+
+        byte entero = 12;
+        short sentero = 12456;
+        int ientero = 12456;
+        long lentero = 12345656;
+        float flotante = 12.12f;
+        double dflotante = 12.1212;
+        char caracter = 'a';
+        boolean decision = true;
+
+        System.out.println("Byte entero de 8 bits "+entero);
+        System.out.println("Short entero de 16 bits "+ sentero);
+        System.out.println("Int entero de 32 bits " + ientero);
+        System.out.println("Long entero de 64 bits " + lentero);
+        System.out.println("Float flotante con una F al final y de 32 bits " + flotante);
+        System.out.println("Double dflotante sin f al final y de 64 bits " + dflotante);
+        System.out.println("Char caracter con comillas simples " + caracter);
+        System.out.println("Boolean solo puede ser true or false " + decision);
+    }
+}
